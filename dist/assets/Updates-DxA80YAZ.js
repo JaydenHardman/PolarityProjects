00167b9c142bb3fa07a3caab5b8b5c899db79949
JaydenHardman <Jayden.Hardman.work@gmail.com>
2024-09-20 22:50:59 +0200

    Deployment to Github Pages

---
+++ dist/assets/Updates-DxA80YAZ.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as s}from"./index-D6Ua8ZIu.js";const o={name:"Updates",data(){return{updates:[]}},methods:{},computed:{},created(){},mounted(){}},a={class:"d-flex justify-center align-center"};function c(n,r,d,p,i,_){return s(),t("div",a," Updates Will be posted shortly. Expect to see some amazing things. ")}const m=e(o,[["render",c]]);export{m as default};
